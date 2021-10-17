68b9c4439f2fdf54ac5a9b7a7cf292b98154b011
DolphinSu <dolphinsue@mail2000.com.tw>
2021-10-17 17:14:10 +0800

    corrects targets.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "KDAnimatedImage",
-            targets: ["KDAnimatedImage"]),
+            targets: ["KDAnimatedImageFramework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +18,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "KDAnimatedImage",
-            dependencies: []),
-        .testTarget(
-            name: "KDAnimatedImageTests",
-            dependencies: ["KDAnimatedImage"]),
+//        .target(
+//            name: "KDAnimatedImage",
+//            dependencies: []),
+//        .testTarget(
+//            name: "KDAnimatedImageTests",
+//            dependencies: ["KDAnimatedImage"]),
+        .binaryTarget(name: "KDAnimatedImageFramework",
+                      path: "Sources/KDAnimatedImageFramework/archives/KDAnimatedImage.xcframework")
     ]
 )
